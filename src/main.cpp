cd775c371df3326d969608bbd914be85378ebcf4
杨培文 (Yang Peiwen) <915505626@qq.com>
2024-08-26 11:11:26 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -34,38 +34,51 @@ using json = nlohmann::json;
 string navigationUrl = "";
 
 void __wait() {
-    while(true) {
+    while (true)
+    {
         this_thread::sleep_for(20000ms);
     }
 }
 
 void __startApp() {
     json options = settings::getConfig();
-    switch(settings::getMode()) {
-        case settings::AppModeBrowser:
-            os::open(navigationUrl);
-            __wait();
-            break;
-        case settings::AppModeWindow: {
-            json windowOptions = options["modes"]["window"];
-            windowOptions["url"] = navigationUrl;
-            window::controllers::init(windowOptions);
-            }
-            break;
-        case settings::AppModeCloud:
-            if(neuserver::isInitialized()) {
-                debug::log(debug::LogTypeInfo, settings::getAppId() +
-                        " is available at " + navigationUrl);
-            }
-            __wait();
-            break;
-        case settings::AppModeChrome: {
-            json chromeOptions = options["modes"]["chrome"];
-            chromeOptions["url"] = navigationUrl;
-            chrome::init(chromeOptions);
-            __wait();
-            }
-            break;
+
+    if (options.contains("startEXE"))
+    {
+        string startEXE = options["startEXE"];
+        string a;
+        os::execCommand(startEXE, a, true);
+    }
+
+    switch (settings::getMode())
+    {
+    case settings::AppModeBrowser:
+        os::open(navigationUrl);
+        __wait();
+        break;
+    case settings::AppModeWindow:
+    {
+        json windowOptions = options["modes"]["window"];
+        windowOptions["url"] = navigationUrl;
+        window::controllers::init(windowOptions);
+    }
+    break;
+    case settings::AppModeCloud:
+        if (neuserver::isInitialized())
+        {
+            debug::log(debug::LogTypeInfo, settings::getAppId() +
+                " is available at " + navigationUrl);
+        }
+        __wait();
+        break;
+    case settings::AppModeChrome:
+    {
+        json chromeOptions = options["modes"]["chrome"];
+        chromeOptions["url"] = navigationUrl;
+        chrome::init(chromeOptions);
+        __wait();
+    }
+    break;
     }
 }
 
@@ -74,27 +87,30 @@ void __configureLogger() {
     bool enableLogFile = true;
 
     json logging = settings::getOptionForCurrentMode("logging");
-    if(!logging["enabled"].is_null()) {
+    if (!logging["enabled"].is_null())
+    {
         enableLogging = logging["enabled"].get<bool>();
     }
-    if(!logging["writeToLogFile"].is_null()) {
+    if (!logging["writeToLogFile"].is_null())
+    {
         enableLogFile = logging["writeToLogFile"].get<bool>();
     }
 
     el::Configurations defaultConf;
     defaultConf.setToDefault();
     defaultConf.setGlobally(
-            el::ConfigurationType::Format, NEU_APP_LOG_FORMAT);
+        el::ConfigurationType::Format, NEU_APP_LOG_FORMAT);
 
-    if(enableLogFile) {
+    if (enableLogFile)
+    {
         defaultConf.setGlobally(
-                el::ConfigurationType::Filename, settings::joinAppPath(NEU_APP_LOG_FILE));
+            el::ConfigurationType::Filename, settings::joinAppPath(NEU_APP_LOG_FILE));
     }
     defaultConf.setGlobally(
-            el::ConfigurationType::ToFile, enableLogFile ? "true" : "false");
+        el::ConfigurationType::ToFile, enableLogFile ? "true" : "false");
 
     defaultConf.setGlobally(
-            el::ConfigurationType::Enabled, enableLogging ? "true" : "false");
+        el::ConfigurationType::Enabled, enableLogging ? "true" : "false");
     el::Loggers::reconfigureLogger("default", defaultConf);
 }
 
@@ -102,14 +118,18 @@ void __startServerAsync() {
     navigationUrl = settings::getNavigationUrl();
     json jEnableServer = settings::getOptionForCurrentMode("enableServer");
 
-    if(!jEnableServer.is_null() && jEnableServer.get<bool>()) {
-        try {
+    if (!jEnableServer.is_null() && jEnableServer.get<bool>())
+    {
+        try
+        {
             navigationUrl = neuserver::init();
         }
-        catch(websocketpp::exception &e) {
+        catch (websocketpp::exception& e)
+        {
             json jPort = settings::getOptionForCurrentMode("port");
             string errorMsg = "Neutralinojs can't initialize the application server";
-            if(!jPort.is_null()) {
+            if (!jPort.is_null())
+            {
                 errorMsg += " on port: " + to_string(jPort.get<int>());
             }
             pfd::message("Unable to start server",
@@ -122,11 +142,12 @@ void __startServerAsync() {
     }
 }
 
-void __initFramework(const json &args) {
+void __initFramework(const json& args) {
     settings::setGlobalArgs(args);
     resources::init();
     bool settingsStatus = settings::init();
-    if(!settingsStatus) {
+    if (!settingsStatus)
+    {
         pfd::message("Unable to load configuration",
             "The application configuration file cannot be loaded due to a JSON parsing error.",
             pfd::choice::ok,
@@ -141,18 +162,22 @@ void __initExtra() {
     bool enableExtensions = false;
     bool exportAuthInfo = false;
     json exts = settings::getOptionForCurrentMode("enableExtensions");
-    if(!exts.is_null()) {
+    if (!exts.is_null())
+    {
         enableExtensions = exts.get<bool>();
     }
     json exportAuth = settings::getOptionForCurrentMode("exportAuthInfo");
-    if(!exportAuth.is_null()) {
+    if (!exportAuth.is_null())
+    {
         exportAuthInfo = exportAuth.get<bool>();
     }
 
-    if(exportAuthInfo) {
+    if (exportAuthInfo)
+    {
         authbasic::exportAuthInfo();
     }
-    if(enableExtensions) {
+    if (enableExtensions)
+    {
         extensions::init();
     }
 }
@@ -160,7 +185,8 @@ void __initExtra() {
 #if defined(_WIN32)
 void __attachConsole() {
     FILE* fp;
-    if(AttachConsole(ATTACH_PARENT_PROCESS)) { 
+    if (AttachConsole(ATTACH_PARENT_PROCESS))
+    {
         freopen_s(&fp, "CONIN$", "r", stdin);
         freopen_s(&fp, "CONOUT$", "w", stdout);
         freopen_s(&fp, "CONOUT$", "w", stderr);
@@ -171,23 +197,24 @@ void __attachConsole() {
 #define ARG_V __wargv
 #define CONVWCSTR(S) helpers::wcstr2str(S)
 int APIENTRY wWinMain(HINSTANCE hInstance,
-                      HINSTANCE hPrevInstance,
-                      LPTSTR    lpCmdLine,
-                      int       nCmdShow)
+    HINSTANCE hPrevInstance,
+    LPTSTR    lpCmdLine,
+    int       nCmdShow)
 #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__)
 #define ARG_C argc
 #define ARG_V argv
 #define CONVWCSTR(S) S
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 #endif
-                                 {
+{
     json args;
-    for (int i = 0; i < ARG_C; i++) {
+    for (int i = 0; i < ARG_C; i++)
+    {
         args.push_back(CONVWCSTR(ARG_V[i]));
     }
-    #if defined(_WIN32)
+#if defined(_WIN32)
     __attachConsole();
-    #endif
+#endif
     __initFramework(args);
     __startServerAsync();
     __configureLogger();
