46d7108b1d91cd41fd3d2bd1f22687e749c23ff8
杨培文 (Yang Peiwen) <915505626@qq.com>
2024-09-10 17:39:39 +0800

    Update 换图标.bat

--- 换图标.bat
+++ 换图标.bat
@@ -1,2 +1,8 @@
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "bin/neutralino-win_x64.exe" -save "bin/neutralino-win_x64.exe" -action addoverwrite -res "ico.ico" -mask ICONGROUP,MAINICON,0
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV AI Platform-win_x64.exe" ^
+-action addoverwrite -res "ico.ico" -mask ICONGROUP,MAINICON,0
+
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open dist/env.rc -save dist/env.res -action compile -log console
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "dist/DLCV AI Platform-win_x64.exe" -save "dist/DLCV AI Platform-win_x64.exe" ^
+-action addoverwrite -res "dist/env.res" -mask VERSIONINFO
+
 pause
